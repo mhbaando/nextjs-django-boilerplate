9c9be92899143ab1ce994ff7bd1d7c0985d21041
mhbaando <maxamuudyare21@gmail.com>
2025-12-01 12:47:23 +0300

    feat: Render change password component

--- app/(auth)/change-password/page.tsx
+++ app/(auth)/change-password/page.tsx
@@ -1,3 +1,4 @@
+import ChangePasswordComponent from "@/components/auth/change-password";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -6,5 +7,9 @@ export const metadata: Metadata = {
 };
 
 export default function Page() {
-  return <div></div>;
+  return (
+    <div className="w-full h-full">
+      <ChangePasswordComponent />
+    </div>
+  );
 }
