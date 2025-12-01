b2a25c4d1b63f8b0b9a2afe45d81b96b214a949a
mhbaando <maxamuudyare21@gmail.com>
2025-12-01 12:49:10 +0300

    feat: Add Verify OTP component
    - Added the VerifyOtpComponent to the page.
    - Wrapped the component in a div for layout.

--- app/(auth)/verify-otp/page.tsx
+++ app/(auth)/verify-otp/page.tsx
@@ -1,10 +1,14 @@
 import { Metadata } from "next";
-
+import VerifyOtpComponent from "@/components/auth/verifyOtp";
 export const metadata: Metadata = {
   title: "Verify OTP",
   description: "Verify OTP to access your account",
 };
 
 export default function Page() {
-  return <div></div>;
+  return (
+    <div className="w-full h-full">
+      <VerifyOtpComponent />
+    </div>
+  );
 }
